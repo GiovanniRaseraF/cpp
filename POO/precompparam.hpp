c59bc2fa7a03bc619992a18d01ea6318f5cb06c2
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-10-27 18:00:50 +0200

    POO: eliminazione di codice duplicato

--- POO/precompparam.hpp
+++ POO/precompparam.hpp
@@ -1,2 +1,34 @@
-// Abilita il debug 
-//#define DEB
+#pragma once
+#include "precompparam.hpp"
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+#ifdef DEB
+#define PRINT(str) debugPrint(str);
+#define PRINTLN(str) debugPrintln(str);
+
+void debugPrint(const char *const str){
+    std::cout << str;
+}
+void debugPrint(std::string str){
+    std::cout << str;
+}
+void debugPrintln(const char *const str){
+    std::cout << str << std::endl;
+}
+void debugPrint(double str){
+    std::cout << str;
+}
+void debugPrintln(double str){
+    std::cout << str << std::endl;
+}
+void debugPrintln(std::string str){
+    std::cout << str << std::endl;
+}
+
+#else
+#define PRINT(str) ;
+#define PRINTLN(str) ;
+#endif
