b08edb21b19a1b541b6bc08dc98ec26081598b9a
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-11-14 15:12:13 +0100

    io: challange 2 done

--- io/challange/ch2.cpp
+++ io/challange/ch2.cpp
@@ -2,7 +2,57 @@
 #include <string>
 #include <iomanip>
 #include <fstream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+int countcorrect(std::string &corretta, std::string &data){
+    size_t lencor = std::min(corretta.length(), data.length());
+    int punti{0};
+
+    for(size_t i = 0; i < lencor; i++)
+        if(std::tolower(corretta[i]) == std::tolower(data[i]))
+            punti++;
+
+    return punti;
+}
+
+void stampa(std::map<std::string, int> &risposte, double avg){
+    // Stampa
+    std::cout << std::setw(15) << std::left << "Student" << std::setw(10) << std::right << "Score" << std::endl;
+    std::cout << std::setw(25) << std::setfill('-') << "" << std::endl << std::setfill(' ');
+    for(auto &i : risposte){
+        std::cout << std::setw(15) << std::left << i.first << std::setw(10) << std::right << i.second << std::endl;
+    }
+    std::cout << std::setw(25) << std::setfill('-') << "" << std::endl << std::setfill(' ');
+    std::cout << std::setw(15) << std::left << "Average" << std::setw(10) << std::right << avg << std::endl;
+}
 
 int main(){
-    
+    std::map<std::string, int> risposte{};
+    double sumpoints{0};
+    double avg{0};
+    std::string rispostaesatta{};
+    std::ifstream input{"./inputch2.txt"};
+
+    if(!input){
+        std::cerr << "Non posso aprire il file" << std::endl;
+        std::exit(1);
+    }
+
+    // Carica la prima riga
+    input >> rispostaesatta;
+
+    std::string nome{}, risposta{};
+    while(!(input >> nome >> risposta).eof()){
+        risposte[nome] = countcorrect(rispostaesatta, risposta);
+        sumpoints += risposte[nome];
+    }
+    avg = sumpoints / risposte.size(); 
+
+    stampa(risposte, avg);
+
+    input.close();
 }
