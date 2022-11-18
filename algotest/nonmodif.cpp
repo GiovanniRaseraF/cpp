d0d64f30cbf5c94c2fd89f2d0b7df0280a04b919
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-11-18 19:58:55 +0100

    stl: algos: testing di funzionalità sui container utilizzando la libreria <algorithm>

---
+++ algotest/nonmodif.cpp
@@ -0,0 +1,60 @@
+#include "test.hpp"
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <string>
+
+template <typename T>
+void printto(std::ostream &os, std::vector<T> &vet, std::string name){
+    os << std::setw(10) << std::left << name+":" << "[";
+    std::for_each(vet.begin(), vet.end(), [&os](const T &v){
+        os << std::setw(3) << v << " "; 
+    });
+    os << "]";
+}
+
+int main(){
+    std::cout << std::setw(40) << "Test di all_of, any_of, none_of" << std::endl;
+    std::vector<int> values1{1, 2, 3, 4, 5, 6, 89, 100, 7, 12, 2, 4, 5};
+
+    // Controllo se il predicato vale per tutti i valori del mio vector
+    printto(std::cout, values1, "values1"); std::cout << std::endl;
+    // all_of: controllo che tutti siano pari
+    TEST(
+        std::all_of(values1.begin(), values1.end(), [](const int v){return v * 2 == 0;}), 
+        false,
+        "all_of: non sono tutti pari"
+    );
+
+    // any_of: controllo che almeno uno sia lungo 3 lettere
+    TEST(
+        std::any_of(values1.begin(), values1.end(), [](const int v){
+            return v / 100 >= 1;
+        }), 
+        true,
+        "any_of: un elemento è lungo tre numeri: il 100"
+    );
+
+    // none_of: controllo che nessuno sia divisibile per 123
+    TEST(
+        std::none_of(values1.begin(), values1.end(), [](const int v){
+            return v * 123 == 0;
+        }),
+        true,
+        "none_of: nessuno degli elementi è divisibile per 123"
+    );
+
+    // none_of: controllo che nessuno sia divisibile per 2
+    TEST(
+        std::none_of(values1.begin(), values1.end(), [](const int v){
+            return v % 2 == 0;
+        }),
+        false,
+        "none_of: almeno un elemento è divisibile per 2"
+    );
+    
+
+}
---
+++ algotest/test.hpp
@@ -0,0 +1,89 @@
+#pragma once
+///
+/// Author: Giovanni Rasera
+/// Link: https://github.com/GiovanniRaseraF
+///
+/// Description: Simple test and debug header
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <chrono>
+
+#ifdef VEC_PRINT
+template <class T>
+std::ostream &operator<<(std::ostream& os, std::vector<T> &vec){
+    for(T val : vec){
+        os << std::setw(4) << val;
+    }
+
+    return os;
+}
+
+template <class T>
+std::ostream &operator<<(std::ostream& os, std::vector<T> &&vec){
+    for(T val : vec){
+        os << std::setw(4) << val;
+    }
+
+    return os;
+}
+#endif
+
+#define TEST(func, expected, error)\
+{\
+    if(func != expected){\
+        std::cerr << std::setw(std::strlen(error) + 7) << std::setfill('-') << "";std::cerr << std::setfill(' ');\
+        std::cerr << std::endl << "ERROR: "<< error << "" << std::endl;\
+        std::cerr << std::boolalpha;\
+        std::cerr << std::setw(15) << std::left << " -expected:" << expected << std::endl;\
+        std::cerr << std::setw(15) << std::left << " -got:" << func << std::endl;\
+        std::cerr << std::noboolalpha;\
+       	std::cerr << std::setw(std::strlen(error) + 7) << std::setfill('-') << "" << std::endl;std::cerr << std::setfill(' ');\
+    }\
+    else{\
+        std::cerr << std::setw(std::strlen(error) + 8) << std::setfill('-') << "" << std::endl;\
+        std::cerr << "PASSED: "<< error << "" << std::endl;std::cerr << std::setfill(' ');\
+        std::cerr << std::setw(std::strlen(error) + 8) << std::setfill('-') << "" << std::endl;\
+    }\
+}
+#define CHRONO_TEST(func, message)\
+{\
+    std::cout << std::setw(std::strlen(message) + 10) << std::setfill('-') << "" << std::endl;std::cout << std::setfill(' ');\
+    std::cout << "CHRONING: " << message << std::endl;\
+    const auto start = std::chrono::high_resolution_clock::now();\
+    func;\
+    const auto end = std::chrono::high_resolution_clock::now();\
+    const std::chrono::duration<double, std::milli> ms = end-start;\
+    std::cout << std::setw(15) << std::left << " -Duration: " << ms.count() << "ms" << std::endl;\
+    std::cout << std::setw(std::strlen(message) + 10) << std::setfill('-') << "" << std::endl;std::cout << std::setfill(' ');\
+}
+
+#ifdef DEB
+#define PRINT(str) debugPrint(str);
+#define PRINTLN(str) debugPrintln(str);
+
+void debugPrint(const char *const str){
+    std::cout << "DEB:" << str;
+}
+void debugPrint(std::string str){
+    std::cout << "DEB:" << str;
+}
+void debugPrintln(const char *const str){
+    std::cout << "DEB:" << str << std::endl;
+}
+void debugPrint(double str){
+    std::cout << "DEB:" << str;
+}
+void debugPrintln(double str){
+    std::cout << "DEB:" << str << std::endl;
+}
+void debugPrintln(std::string str){
+    std::cout << "DEB:" << str << std::endl;
+}
+
+#else
+#define PRINT(str) ;
+#define PRINTLN(str) ;
+#endif
