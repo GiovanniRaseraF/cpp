9503d381f21873cefec7ca79414b7cd9c85fdf98
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-12-03 18:32:39 +0100

    threads: unique lock with try_lock_for

--- threads/unique_lock.cpp
+++ threads/unique_lock.cpp
@@ -3,28 +3,29 @@
 #include <string>
 #include <iostream>
 
-std::mutex mut_print;
+std::timed_mutex mut_print;
 
 void function(int n){
     for(int i = 0; i < 10; i++){
         try{
             bool schippend = true;
-            std::unique_lock<std::mutex> unq_lock_print{mut_print, std::defer_lock};
-            unq_lock_print.try_lock_for(
-                //std::chrono::time_point<
-                 //   std::chrono::high_resolution_clock>{
-                         std::chrono::seconds(1)
-                    //}
-            );
+            std::unique_lock<std::timed_mutex> unq_lock_print{mut_print, std::defer_lock};
+            unq_lock_print.try_lock_for(std::chrono::seconds(1));
+            //unq_lock_print.try_lock();
+
             schippend = false;
             std::cout << "num: " << n << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(5));
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
             unq_lock_print.unlock();
+
             if (schippend){
                 std::cout << "skippend the lock" << std::endl;
+            }else{
+                std::cout << "unlocked correctly" << std::endl;
             }
-        }catch(int &ex){
-            std::cout << ex << std::endl;
+        }catch(std::system_error &ex){
+            std::cout << ex.what() << std::endl;
         }
     }
 }
