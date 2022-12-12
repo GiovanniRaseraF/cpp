6df4c92ce319bf41e2ad000253e9df9e43efb877
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-12-12 16:16:24 +0100

    condvars: condition with predicate, NOT WORKING

---
+++ conditionvariables/condwithpredicate.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <string>
+
+// per evitare che il wakup venga chiamato prima del wait
+// devo usare una funzione nel wait
+
+using namespace std::chrono_literals;
+
+// Globals
+std::mutex mut;
+std::condition_variable cv;
+
+std::string sdata{"empty"};
+bool condition{true};
+
+// Waiting thread
+void reader(){
+    std::cout << "reading thread starts" << std::endl;
+    //std::this_thread::sleep_for(1s);
+    while(true){
+        {
+        std::unique_lock<std::mutex> lk{mut};
+        cv.wait(lk, []{return condition;});
+
+        std::cout << "data is: " << sdata << " " << std::endl;
+        condition = false;
+        }
+    }
+}
+
+void writer(){
+    std::cout << "writer init" << std::endl;
+    int index = 0;
+
+    while(true){
+        index++;
+        {
+            std::lock_guard<std::mutex> lg{mut};
+            std::this_thread::sleep_for(1s);
+
+            sdata = "hello man " + std::to_string(index);
+            condition = true;
+        }
+        cv.notify_one();
+    }
+
+    std::cout << "writer exit";
+}
+
+int main(){
+    std::thread t1{reader};
+    std::thread t2{writer};
+
+    t1.join();
+    t2.join();
+}
