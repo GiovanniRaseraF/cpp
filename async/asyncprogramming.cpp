b3738559be731597e4953b605779644e9d6d533b
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-12-18 16:32:03 +0100

    async: started with std::packaged_task

---
+++ async/asyncprogramming.cpp
@@ -0,0 +1,26 @@
+/*/
+Le operazini possonon essere esequite in modo 
+- syncrono: 
+    - aspetto che una finisca per cominciare la prossima
+- asyncrono:
+    - eseguo le oprazioni senza aspettare nello stesso momento 
+
+
+synchronous programming
+- se comincio con un task devo aspettare che questo finisca prima di procedere 
+e fare altro
+
+ho dei task che sono syncronous in c++ 
+
+data.save(filename)
+
+Async programming
+
+creo un task che fanno cose in background
+
+
+___
+bisogna cercare di minimizzare il blocking
+
+*/
+
---
+++ async/packaged_task.cpp
@@ -0,0 +1,14 @@
+/*
+Utilizzo std::packaged_task che offre un'astrazione di piu alto livello 
+
+Associa una funzione con un std::promise
+
+un std::packaged_task rappresenta un'operazione asincrona
+    - quando viene chiamata e esegueta la funzione il risultato viene salvano in una
+    promise
+
+    - è possibile avere il valore ritornato grazie alla future associata
+
+
+
+/*/
