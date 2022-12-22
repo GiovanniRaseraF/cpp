721ed2f13ec482ce30f6d2932ce62d7770c8060e
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-12-22 08:31:04 +0100

    async: challange timings with wait() on the future

--- async/challangetimings.cpp
+++ async/challangetimings.cpp
@@ -8,7 +8,7 @@
 #include <chrono>
 
 int accum(std::vector<int>::iterator beg, std::vector<int>::iterator end, int init) {
-    return std::reduce(beg, end, init);
+    return std::accumulate(beg, end, init);
 }
 
 int main(){
@@ -20,24 +20,37 @@ int main(){
     task3{accum},
     task4{accum};
 
-    std::future<int> f1{task1.get_future()}, f2{task2.get_future()}, f3{task3.get_future()}, f4{task4.get_future()};
+    std::future<int> 
+    f1{task1.get_future()}, 
+    f2{task2.get_future()}, 
+    f3{task3.get_future()}, 
+    f4{task4.get_future()};
 
-    std::cout << "Calcolo" << std::endl;
-    const std::chrono::time_point<std::chrono::steady_clock> start =
-        std::chrono::steady_clock::now();
+    std::cout << "Calculation" << std::endl;
+    const auto start = std::chrono::steady_clock::now();
+
     std::thread 
     t1{std::move(task1), vet.begin(), vet.begin()+30'000'000, 0},
     t2{std::move(task2), vet.begin()+30'000'000, vet.begin()+60'000'000, 0},
     t3{std::move(task3), vet.begin()+60'000'000, vet.begin()+90'000'000, 0},
     t4{std::move(task4), vet.begin()+90'000'000, vet.end(), 0};
-    
     t1.join();
     t2.join();
     t3.join();
     t4.join();
 
+    // If i wait for result the code runs faster, 
+    // O0 optimizazion
+    // wait: 4856910 us, 4856910623 ns, 4856 ms, 4 s 
+    // NO wait: 5029281 us, 5029281069 ns, 5029 ms, 5 s
+    // O3 optimization
+    // wait: 173243 us, 173243327 ns, 173 ms, 0 s
+    // NO wait: 204923 us, 204923246 ns, 204 ms, 0 s 
+    f1.wait(); f2.wait(); f3.wait(); f4.wait();
     int res = f1.get() + f2.get() + f3.get() + f4.get();
+
     const auto end = std::chrono::steady_clock::now();
+
     std::cout << 
     std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us, " <<
     std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns, " << 
