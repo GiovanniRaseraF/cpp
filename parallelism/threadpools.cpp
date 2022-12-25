b79391276a7936e9e5ff0df2d79e7ce1250dfab9
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-12-25 11:11:13 +0100

    parallelism: adding thread pool tests

--- parallelism/threadpools.cpp
+++ parallelism/threadpools.cpp
@@ -15,20 +15,21 @@ class concurrent_queue{
     concurrent_queue() = default;
     void push(T val){
         std::lock_guard<std::mutex> lg(m);
-        q.push(value);
+        q.push(val);
     }
 
     void pop(T &val){
         std::unique_lock<std::mutex> lg(m);
 
         if (q.empty()){
-            throw std::exception("queue is empty");
+            throw std::exception();
         }
 
         val = q.front();
         q.pop();
     }
 };
+
 using func = std::function<void()>;
 class thpoll{
     concurrent_queue<func> work_q;
@@ -43,16 +44,24 @@ class thpoll{
 thpoll::thpoll(){
     auto numcpus = std::thread::hardware_concurrency();
     for(int i = 0; i < numcpus; i++){
-        threads.push_back(std::thread{thpoll::worker, this});
+        threads.push_back(std::thread{&thpoll::worker, this});
     }
 }
 
-
+thpoll::~thpoll(){
+    for(auto &t : threads)
+        t.join();
+}
 
 void thpoll::worker(){
     while(true){
         func work;
-        work_q.pop(work);
+        try{
+            work_q.pop(work);
+        }catch(...){
+            //std::cout << "end";
+            return;
+        }
 
         work();
     }
@@ -60,4 +69,27 @@ void thpoll::worker(){
 
 void thpoll::submit(func f){
     work_q.push(f);
+}
+
+int main(){
+    thpoll poll{};
+
+    for(int i = 0; i < 6; i++){
+        poll.submit([](){
+            std::cout << "" << std::this_thread::get_id() << std::endl;
+        });
+    }
+
+    for(int i = 0; i < 10; i++){
+        poll.submit([](){
+            std::vector<int> vv{1, 3, 4, 8, 2, 3};
+            int val = (std::rand() * 100) % vv.size();
+            std::this_thread::sleep_for(std::chrono::seconds(
+               val 
+            ));
+            std::cout << "end" << std::endl;
+        });
+    }
+
+    return 0;
 }
