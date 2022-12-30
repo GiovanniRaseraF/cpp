7dee14995b4e2f2bb4541d217528fcafbe7bec97
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-12-30 14:50:10 +0100

    kalman: init

---
+++ kalmanfilter/kalman.cpp
@@ -0,0 +1,9 @@
+/*
+Perche si usano i filtri di kalman
+
+- data fusion è un strumento fondamentale che viene utilizzato in ogni tecnologia moderna
+- Il filtro di kalman è il motodo di data fusion più utilizzato 
+- utilizzato con i droni e con le macchine che guidano da sele
+
+- 
+*/
