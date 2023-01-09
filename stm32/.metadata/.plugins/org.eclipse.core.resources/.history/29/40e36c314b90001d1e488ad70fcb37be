ef61c6220930a3acc64f1bb18a43bbb495fffd49
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2023-01-09 19:28:04 +0100

    stm32: added printer and c++ compilation for freertos and usb comunicatio

---
+++ stm32/.metadata/.plugins/org.eclipse.core.resources/.history/29/40e36c314b90001d1e488ad70fcb37be
@@ -0,0 +1,18 @@
+/*
+ * printer.cpp
+ *
+ *  Created on: Jan 9, 2023
+ *      Author: giovannirasera
+ */
+
+#include "printer.h"
+
+printer::printer() {
+	// TODO Auto-generated constructor stub
+	int a = 0;
+}
+
+printer::~printer() {
+	// TODO Auto-generated destructor stub
+}
+
--- stm32/usbandfreertos/Debug/sources.mk
+++ stm32/usbandfreertos/Debug/sources.mk
@@ -4,22 +4,32 @@
 ################################################################################
 
 ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
 OBJ_SRCS := 
 S_SRCS := 
+CC_SRCS := 
 C_SRCS := 
+CPP_SRCS := 
 S_UPPER_SRCS := 
 O_SRCS := 
+OBJDUMP_LIST := 
+C_UPPER_DEPS := 
+S_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+CC_DEPS := 
 OBJCOPY_HEX := 
 SIZE_OUTPUT := 
-OBJDUMP_LIST := 
+C++_DEPS := 
 SU_FILES := 
 EXECUTABLES := 
 OBJS := 
+CXX_DEPS := 
 MAP_FILES := 
-S_DEPS := 
 S_UPPER_DEPS := 
-C_DEPS := 
-OBJCOPY_BIN := 
+CPP_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
@@ -34,4 +44,5 @@ Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F \
 Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang \
 USB_DEVICE/App \
 USB_DEVICE/Target \
+Core \
 
