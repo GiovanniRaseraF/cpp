5877664daf17a9317026f1c0ba156facd79d9a11
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2025-07-30 16:14:06 +0200

    main: aggiornamento

--- pytorch/code/run.sh
+++ pytorch/code/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ./bin/activate && jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7'
 # jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7'
-# http://192.168.0.107:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
+# http://192.168.8.108:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
 # http://localhost:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
+# jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7' --ip=192.168.8.108
