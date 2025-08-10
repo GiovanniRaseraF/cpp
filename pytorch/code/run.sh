abf7831ef8a63e7ff170e4b00363bd1587a173f0
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2025-08-10 15:35:39 +0200

    main: update

--- pytorch/code/run.sh
+++ pytorch/code/run.sh
@@ -1,11 +1,12 @@
 source ./bin/activate 
-jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7'
+jupyter lab --port=8888 --no-browser --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7'
 http://localhost:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
 
 ./Scripts/activate.bat
-jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7' --ip=192.168.8.108
+jupyter lab --ip=192.168.8.108 --port=8888 --no-browser --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7' 
 http://192.168.8.108:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
 
 ./Scripts/activate.bat
-jupyter lab --port=8888 --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7' --ip=192.168.8.128
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128 --upgrade --force-reinstall
+jupyter lab  --ip=192.168.8.128 --port=8888 --no-browser --ServerApp.token='6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7'
 http://192.168.8.128:8888/lab?token=6c4bfd9ffd59618566557195d0f0a733f246545b8b118ca7
