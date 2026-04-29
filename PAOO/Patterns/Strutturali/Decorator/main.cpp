b8a6670a60a3e179c71a95804547b4236d78d0d2
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2026-04-29 09:41:32 +0200

    update

--- PAOO/Patterns/Strutturali/Decorator/main.cpp
+++ PAOO/Patterns/Strutturali/Decorator/main.cpp
@@ -10,6 +10,7 @@
 class Decorable {
 public:
     std::string name;
+    virtual ~Decorable() = default;
     virtual std::string toString() = 0;
 };
 
@@ -58,7 +59,18 @@ public:
 
 int main(){
     std::cout << "Test cube" << std::endl;
-    Decorable *d = new BorderColor(std::make_shared<SimpleCube>(), "red");
+    std::shared_ptr<Decorable> d = std::make_shared<BorderColor>(std::make_shared<SimpleCube>(), "red");
     std::cout << d->toString() << std::endl;
+
+    // Need to show that this can be done at runtime while using a for loop
+    std::string input;
+    std::shared_ptr<Decorable> first = std::make_shared<SimpleCube>();
+    for(int i = 0; i < 5; i++){
+        std::cout << "Color:>>"; std::cin >> input;
+        first = std::make_shared<BorderColor>(first, input+std::to_string(i));
+        std::cout << first->toString() << std::endl;
+    }
+    std::cout << first->toString() << std::endl;
+
     return 0;
 }
