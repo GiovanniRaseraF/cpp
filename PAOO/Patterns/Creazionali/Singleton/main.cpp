2068e232edac6dae2f5b3ee87fe179abf55675e7
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2026-04-30 11:02:02 +0200

    update

---
+++ PAOO/Patterns/Creazionali/Singleton/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <optional>
+#include <memory>
+#include <fmt/core.h>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+class PrinterDriver {
+private:
+    static std::shared_ptr<PrinterDriver> instance;
+
+    PrinterDriver() {
+        std::cout << "Created new instance";
+    }
+
+public:
+    PrinterDriver(PrinterDriver &) = delete;
+    PrinterDriver(PrinterDriver &&) = delete;
+    void operator=(const PrinterDriver &) = delete;
+
+    static std::shared_ptr<PrinterDriver> getInstance(){
+        if(PrinterDriver::instance == nullptr){
+            instance = std::make_shared<PrinterDriver>();
+        }
+        return instance;
+    }
+};
+// This is necessary 
+std::shared_ptr<PrinterDriver> PrinterDriver::instance = nullptr;
+
+
+int main(){
+    std::shared_ptr<PrinterDriver> p = PrinterDriver::getInstance();
+    std::shared_ptr<PrinterDriver> p2 = PrinterDriver::getInstance();
+
+    std::cout << std::hex << p.get() << " - " << std::hex << p2.get() << std::endl;
+}
