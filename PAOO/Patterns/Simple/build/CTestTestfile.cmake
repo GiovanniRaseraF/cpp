2923e4c1c8efcda2822ff8746813ae05169a0945
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2026-05-07 15:06:49 +0200

    update

--- PAOO/Patterns/Simple/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/giovanni/repos/cpp/PAOO/Patterns/Simple
-# Build directory: /Users/giovanni/repos/cpp/PAOO/Patterns/Simple/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/giovanni/repos/cpp/PAOO/Patterns/Simple/build/mesh_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
